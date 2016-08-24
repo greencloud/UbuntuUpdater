58df3090734eb254801be2dfd2514a4142a532bc
Rasmus van Guido <greencloud@users.noreply.github.com>
2016-08-24 10:45:56 +0800

    Update ub_updater.sh

--- ub_updater.sh
+++ ub_updater.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 clear
-
 # Defined color settings
 RED=$(tput setaf 1 && tput bold)
 GREEN=$(tput setaf 2 && tput bold)
 STAND=$(tput sgr0)
 BLUE=$(tput setaf 6 && tput bold)
-
 
 echo ""
 echo ""
@@ -31,7 +29,6 @@ echo ""
 sleep 5
 clear
 
-
 echo $RED"    >>>>> UPDATING SYSTEM FROM STANDARD REPO <<<<<"
 # Update system
 echo $GREEN""
@@ -43,32 +40,26 @@ echo ""
 sleep 2
 clear
 
-
 echo $RED"    >>>>> Updating ClamScan Anti-virus program <<<<<"
 echo $RED"            --- THIS MIGHT TAKE A WHILE ----"
 # Update Anti-virus
 # If there's no anti-virus found, install ClamAV
 echo $GREEN""
-
 CLAMFILE=/usr/bin/clamscan
 CLAMAVLOG=/var/log/clamav/freshclam.log
-
 if [ ! -f $CLAMFILE ]; then
 	echo $RED"    !!!! NO ANTI-VIRUS INSTALLED - INSTALLING CLAMAV !!!!"
 	echo $STAND""
 	apt-get install -y clamscan clamav-daemon
 fi
-
 if [ -f $CLAMAVLOG ]; then
 	freshclam
 	tail -10 $CLAMAVLOG
 fi
-
 echo "DONE!"
 echo ""
 sleep 5
 clear
-
 
 echo $STAND"    >>>>> RUNNING THE CLEAN UP PROCESS <<<<<"
 # Clean system
@@ -81,35 +72,27 @@ echo ""
 sleep 2
 clear
 
-
 echo $RED"    >>>>> RUNNING VIRUS QUICK SCAN <<<<<"
 echo $RED" ----- This might take a few minutes -----"
 # Virus quick scan
 echo $GREEN""
-
 VIRUSBIN=/home/rasmus/Virus-bin
-
 if [ -d $VIRUSBIN ]; then
 	rm -fR $VIRUSBIN &>/dev/null
 	mkdir $VIRUSBIN &>/dev/null
 else
 	mkdir $VIRUSBIN &>/dev/null
 fi
-
 clamscan -voi --move=$VIRUSBIN /home/rasmus/*
-
 echo "DONE!"
 echo ""
 sleep 2
 clear
 
-
 echo $GREEN"    >>>>> SUCCESS: System is now clean and up-to-date <<<<<"
 echo ""
 echo $STAND""
-
 pause(){
 	read -n1 -rsp $"Press any key to continue or Ctrl+C to exit..."
 }
-
 pause
